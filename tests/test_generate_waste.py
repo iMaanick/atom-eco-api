c111459548bcef3fb5ed19d3c753d14849e24f90
imaanick <nickolaytrusov01@gmail.com>
2024-11-23 15:30:02 +0300

    add: test for generate waste

--- tests/test_generate_waste.py
+++ tests/test_generate_waste.py
@@ -15,3 +15,17 @@ async def test_generate_waste(client: TestClient, mock_organization_gateway, moc
     assert response.json() == {"detail": f"Successfully added 50 of BIO_WASTE waste to organization 1."}
 
     mock_organization_gateway.generate_waste.assert_called_once_with(1, "BIO_WASTE", 50)
+
+
+@pytest.mark.asyncio
+async def test_generate_waste_not_found(client: TestClient, mock_organization_gateway, mock_uow):
+    mock_organization_gateway.get_organization_by_id.return_value = None
+    params = {
+        "waste_type": "BIO_WASTE",
+        "amount": 60
+    }
+    response = client.get(f"/organizations/1/generate_waste/", params=params)
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": f"Organization not found"}
+
